5f1530d7c9124caa5af375ed1925c366c8d22eab
nawazsadiqu <nawazsadiqu@gmail.com>
2026-09-25 22:42:44 +0530

    Fix page scroll position on route changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,26 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
-
 import Portfolio from "./pages/Portfolio";
 import Partners from "./pages/Partners";
 import Contact from "./pages/Contact";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/partners" element={<Partners />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/partners" element={<Partners />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
